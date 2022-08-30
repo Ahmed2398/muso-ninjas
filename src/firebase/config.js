bcba420527ff0e18e07a71e4ce93c9bbaaa68c5f
Ahmed Ashraf <57802370+Ahmed2398@users.noreply.github.com>
2022-08-30 12:23:35 +0200

    v.3

---
+++ src/firebase/config.js
@@ -0,0 +1,28 @@
+//new V9 imports 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAY9xsRISGUyjY1690Vq-KcfCGQTqnOIFc",
+    authDomain: "muso-ninjas-d7124.firebaseapp.com",
+    projectId: "muso-ninjas-d7124",
+    storageBucket: "muso-ninjas-d7124.appspot.com",
+    messagingSenderId: "338158583320",
+    appId: "1:338158583320:web:38c6c9638453c971384d6d"
+};
+  
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+const projectStorge = firebase.storage().ref()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, projectStorge, timestamp }
